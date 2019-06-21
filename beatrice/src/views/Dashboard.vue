978c0d1419c4495c1178975b370131ae7ecf1773
Andrea Leardini <andrea.leardini@nethesis.it>
2019-06-21 14:34:18 +0200

    dante. added headers in project sources

--- beatrice/src/views/Dashboard.vue
+++ beatrice/src/views/Dashboard.vue
@@ -1,3 +1,21 @@
+<!-- 
+Copyright (C) 2019 Nethesis S.r.l.
+http://www.nethesis.it - info@nethesis.it
+ This file is part of Dante project.
+
+ Dante is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License,
+ or any later version.
+
+ Dante is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with Dante.  If not, see COPYING.
+-->
 <template>
   <div class="ui">
     <h1
--- virgilio/apis/methods.go
+++ virgilio/apis/methods.go
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2019 Nethesis S.r.l.
+ * http://www.nethesis.it - info@nethesis.it
+ *
+ * This file is part of Dante project.
+ *
+ * Dante is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License,
+ * or any later version.
+ *
+ * Dante is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Dante.  If not, see COPYING.
+ */
+
 package apis
 
 import (
@@ -231,7 +251,7 @@ func ReadWidget(c *gin.Context) {
 
 		// if it's a pie chart, change structure of output json
 		if chartData.ChartType == "pie" {
-			pieChart := utils.MapPieChart(chartData)
+			pieChart := utils.MapChartToPieChart(chartData)
 			widget = pieChart
 		} else {
 			widget = chartData
